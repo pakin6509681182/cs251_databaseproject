e9ef989372492e19c4fefc657b486dd7832591fc
singharat6509612054 <singharat.mat@dome.tu.ac.th>
2024-04-16 15:07:53 +0700

    basic query for table

---
+++ query/SQLQuery_3.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [User] (
+    userID INT NOT NULL,
+    SSN VARCHAR(17) NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    birthDate DATE NOT NULL,
+    gender VARCHAR(6) NOT NULL,
+    username VARCHAR(30) NOT NULL,
+    PRIMARY KEY (userID),
+    UNIQUE (SSN),
+    UNIQUE (username)
+);
